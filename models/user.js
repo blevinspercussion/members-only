4a423d8ec21726d9e7e55381640095242415e52f
Adam Blevins <blevinspercussion@gmail.com>
2023-11-03 14:00:33 -0500

    Added status field to user model

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
   username: { type: String, required: true },
+  status: { type: String, required: true },
   password: { type: String, required: true },
 });
 
